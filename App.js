8be69aca6faa245e887b08abf666d0039c912665
Susanna <susanna.tuomi@myy.haaga-helia.fi>
2023-01-20 09:25:20 +0200

    Laskin valmis

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [ekaNum, setEkaNum] = useState('');
+  const [tokaNum, setTokaNum] = useState('')
+  const [result, setResult] = useState('');
+  
+
+  const plus = () => {
+    setResult(parseInt(ekaNum) + parseInt(tokaNum))
+  };
+
+  const minus = () => {
+    setResult(parseInt(ekaNum) - parseInt(tokaNum))
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Result: {result} </Text>
+      <TextInput
+     style={styles.input}
+     onChangeText={ ekaNum => setEkaNum(ekaNum)}
+     value={ekaNum}
+     keyboardType="numeric"
+     />
+     <TextInput
+     style={styles.input}
+     onChangeText={tokaNum => setTokaNum(tokaNum)}
+     value={tokaNum}
+        keyboardType="numeric"
+     />
+     <View style={styles.button}>
+      <Button style={{flex:6}} title="+" onPress={plus}/>
+      <View style={{width: 20}} />
+      <Button style= {{flex:6}} title="-" onPress={minus}/>
+      </View>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -17,4 +50,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{ width:200, 
+    borderColor:'grey', 
+    borderWidth: 1
+  },
+  button:{
+    flexDirection: 'row', 
+  alignItems: 'center',
+  justifyContent: 'space-between'
+},
+
+
 });
